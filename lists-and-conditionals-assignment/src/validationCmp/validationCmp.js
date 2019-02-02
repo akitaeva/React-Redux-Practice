6d44d4021f4e06322aa5efa313fb4b9be9ca5394
Alina Kitaeva <akitaeva@gmail.com>
2019-02-02 15:42:35 -0500

    lists-and-conditionals-assignment initial commit

---
+++ lists-and-conditionals-assignment/src/validationCmp/validationCmp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './validationCmp.css'
+
+const validationCmp = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default validationCmp;
