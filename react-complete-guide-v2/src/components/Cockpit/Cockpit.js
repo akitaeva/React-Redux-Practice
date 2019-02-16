fd19cd31f2bd240d81377dc1c128b1c8bef195f9
Alina Kitaeva <akitaeva@gmail.com>
2019-02-15 23:48:53 -0500

    adds useEffect to a functional component

--- react-complete-guide-v2/src/components/Cockpit/Cockpit.js
+++ react-complete-guide-v2/src/components/Cockpit/Cockpit.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
 
-    let assignedClasses = [];
-    let btnClass='';
+  useEffect(() => {                    //   componentDidMount & componentDidUpdate in 1   
+        console.log("[Cockpit.js] useEffect")  
+  // "http" request
+    setTimeout(()=> {
+        alert("Saved data to the cloud!");
+    }, 500);
+      // [] - empty array allows for the one time exxecution on load
+  }, [props.persons]);  // a true array -> field lets control the execution 
+  
+let assignedClasses = [];
+let btnClass='';
 
-    if (props.showPeople){
-     btnClass = classes.Red;
-    }
+if (props.showPeople){
+    btnClass = classes.Red;
+}
 
-    if (props.people.length <= 2) {
-     assignedClasses.push(classes.red);
-    }
-    if (props.people.length <= 1) {
-     assignedClasses.push(classes.bold);
-   }
+if (props.people.length <= 2) {
+    assignedClasses.push(classes.red);
+}
+if (props.people.length <= 1) {
+    assignedClasses.push(classes.bold);
+}
  
 
-    return (
+return (
         <div className= {classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is the dynamic style!</p>
