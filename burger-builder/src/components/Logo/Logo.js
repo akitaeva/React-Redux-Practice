9a28874633150de6ce177f05219623b63068fc78
Alina Kitaeva <akitaeva@gmail.com>
2019-07-11 14:33:30 -0400

    adds logic and styles for logo

--- burger-builder/src/components/Logo/Logo.js
+++ burger-builder/src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import * as Styled from './Styles';
 
-const logo = () => (
-    <Styled.LogoWrapper>
+const logo = ({height, ...props}) => (
+    <Styled.LogoWrapper height={height} >
       <img src={burgerLogo} alt='BurgerBuider'/>
     </Styled.LogoWrapper>   
 );
--- burger-builder/src/components/Logo/Styles.js
+++ burger-builder/src/components/Logo/Styles.js
@@ -4,10 +4,14 @@ export const LogoWrapper = Styled.div`
   background-color: white;
   padding: 8px; 
   border-radius: 10px;
-  height: 75%;
-
+  height: ${({ height }) => height}%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
   img {
     height: 100%;
+    max-width: 90%;
   }
 `;
 
